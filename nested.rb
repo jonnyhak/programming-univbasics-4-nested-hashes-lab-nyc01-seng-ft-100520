0df452153fa4259a558435f447cfa4881df4b8d5
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-14 17:47:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,8 @@ def hopper
       }
     }
     
-    programmer_hash[:grace_hopper]
-    # programmer_hash[:alan_kay][:known_for]
+    return programmer_hash[:grace_hopper]
+    return programmer_hash[:alan_kay][:known_for]
 
 end
 
